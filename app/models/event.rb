a05ff755a102a83172d310d0c525a20982484b62
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-08-30 23:22:32 +0000

    Add association between Event and Attendance models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
+  has_many :attendances, foreign_key: :created_event_id
 
   validates :title, presence: true
   validates :description, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users
   resources :events
   
+  get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
