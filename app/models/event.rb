f143950ff7ee7ba1d146a3c5a6ca454109f1b7d1
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-08-31 20:16:11 +0000

    Modify Event and User models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
-  has_many :attendances, foreign_key: :created_event_id
+  has_many :invitations, foreign_key: :created_event_id, dependent: :destroy
+  has_many :attendees, through: :invitations, source: :attendee, dependent: :destroy
 
   validates :title, presence: true
   validates :description, presence: true
