aebbaaba07d96754118a4ff5cefe31ae93966a69
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-08-31 20:18:10 +0000

    Add #attend-event and unattend_event methods

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
-  before_action :require_user, only: [:new, :create]
+  before_action :require_user, only: [:new, :create, :attend_event]
+  before_action :set_event, only: [:attend_event, :show]
   def new
     @event = current_user.created_events.build
   end
@@ -9,37 +10,49 @@ class EventsController < ApplicationController
   end
 
   def show
-    @event = Event.find(params[:id])
   end
 
   def create
     @event = current_user.created_events.build(event_params)
     if @event.save
       flash[:notice] = 'Event successfully created'
-      redirect_to @event
+      
     else
       flash[:alert] = 'Something went wrong. Check and try again'
       render :new
     end
   end
 
-  def attend_event
-    redirect_to login_path if session[:user_id].nil?
-    @event = Event.find(params[:id])
-    registration = @event.attendances.build
-    registration.attendee_id = current_user.id
-    if registration.save
-      flash[:notice] = 'Thanks you for registering to attend this event'
-      redirect_to event_path(@event)
+  def attended_event
+    event = Event.find(params[:idz])
+    # if @current_user.attended_events.include?(event)
+    #   flash[:danger] = 'This event is already in your events list! Please try next one.'
+    # else
+    if event.date < Date.today
+      flash[:alert] = 'This event has already ended. Please try next one.'
+      redirect_to event_path(event)
     else
-      flash.now[:alert] = 'Your registration for the event was unsuccessful. Plese try again'
+      current_user.attended_events << event
+      flash[:notice] = 'Event added successfully to your events list. Don\'t forget to attend.'
       redirect_to root_path
     end
+
+  end
+
+  def unattend_event
+    event = Event.find(params[:idz])
+    current_user.attended_events.delete(event)
+    flash[:info] = 'Event successfully removed from your events list.'
+    redirect_to root_path
   end
 
   private
 
   def event_params
     params.require(:event).permit(:title, :description, :date)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
   end
 end
