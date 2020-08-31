df53d5677d2e36d105d7516798201dc56cdb6502
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-08-31 20:14:44 +0000

    Generate model Invitation

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
   resources :users
   resources :events
   
+  post 'add_events_attending', to: 'events#attended_event'
+  post 'rem_events_attending', to: 'events#unattend_event'
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
- root 'events#index'
+  root 'events#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_30_230941) do
-
-  create_table "attendances", force: :cascade do |t|
-    t.integer "attendee_id", null: false
-    t.integer "created_event_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["attendee_id"], name: "index_attendances_on_attendee_id"
-    t.index ["created_event_id"], name: "index_attendances_on_created_event_id"
-  end
+ActiveRecord::Schema.define(version: 2020_08_31_124856) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -31,6 +22,16 @@ ActiveRecord::Schema.define(version: 2020_08_30_230941) do
     t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
+  create_table "invitations", force: :cascade do |t|
+    t.integer "attendee_id"
+    t.integer "created_event_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["attendee_id", "created_event_id"], name: "index_invitations_on_attendee_id_and_created_event_id"
+    t.index ["attendee_id"], name: "index_invitations_on_attendee_id"
+    t.index ["created_event_id"], name: "index_invitations_on_created_event_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -38,7 +39,5 @@ ActiveRecord::Schema.define(version: 2020_08_30_230941) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "attendances", "events", column: "created_event_id"
-  add_foreign_key "attendances", "users", column: "attendee_id"
   add_foreign_key "events", "users", column: "creator_id"
 end
