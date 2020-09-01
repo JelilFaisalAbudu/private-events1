ce2c8521818c9cad1645ffa5020db243ec4eb221
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-09-01 05:33:32 +0000

    Remove all logics into #sesion_check helper EventHelper

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,13 @@
 module EventsHelper
+  def session_check
+    if logged_in?
+      render 'shared/logged_in'
+    else
+      render 'shared/not_logged_in'
+    end
+  end
+
+  def errors_check
+    render './errors' if @user.errors.any?
+  end
 end
