39083c1658e600af38c2d4a27f603b6f1bccd82f
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-09-03 10:29:41 +0000

    Modify my_created_event method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,10 @@
 module UsersHelper
-  def user_activities
+  def my_created_events
     if current_user.created_events.count.zero?
       content_tag(:h2, 'You have not organized any event yet', class: 'text-center')
     else
       render 'partials/user_events'
     end
   end
+
 end
